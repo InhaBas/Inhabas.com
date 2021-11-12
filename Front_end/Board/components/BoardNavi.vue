d247cdc0e816cb287e60f8bc01f4bb88384aa2f2
Yejin <yyj11kr@gmail.com>
2021-11-12 20:18:36 +0900

    [config/#85] Vue 파일 구조 정리 (Board)
    - Board폴더의 하위폴더 및 vue 파일 정의.
    
    resolves: #85

---
+++ Front_end/Board/components/BoardNavi.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "BoardNavi.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front_end/Board/components/BoardSearch.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "BoardSearch.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
